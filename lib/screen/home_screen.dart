50d79364d86781186ad402dc779fd4f1f715da03
Juyeon Kim <kkjuyeon@gmail.com>
2023-09-27 01:39:53 +0900

    feat: 메시지 입력 alert 창 및 디자인 수정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -14,22 +14,36 @@ class _HomeScreenState extends State<HomeScreen> {
     DateTime.now().month,
     DateTime.now().day,
   );
+  final msgController = TextEditingController();
+  String _message = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink[200],
       body: SafeArea(
         bottom: false,
+        top: false,
         child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: NetworkImage(
+              // -- 이미지 링크 삽입
+              'https://plus.unsplash.com/premium_photo-1671578324515-b7c3a68c0002?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            ),
+            fit: BoxFit.cover, // -- 화면 너비 fit
+            opacity: 0.8,
+          )),
           width: MediaQuery.of(context).size.width,
           child: Column(
             children: [
-              _TopPart(
+              _TopPart(),
+              _MiddlePart(
                 selectedDate: selectedDate,
                 onPressed: onHeartPressed,
+                onTapMsg: onTapMsg,
+                message: msgController.text,
               ),
-              _BottomPart(),
+              _BottomPart()
             ],
           ),
         ),
@@ -68,15 +82,80 @@ class _HomeScreenState extends State<HomeScreen> {
       },
     );
   }
+
+  void _updateMsg() {
+    setState(() {
+      _message = msgController.text;
+    });
+  }
+
+  void onTapMsg() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('메시지를 입력해주세요.'),
+          content: Container(
+            color: Colors.white,
+            height: 100.0,
+            child: TextField(
+              controller: msgController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: '메시지',
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text('확인'),
+              onPressed: () {
+                _updateMsg();
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('취소'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
 
 class _TopPart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Expanded(
+        flex: 2,
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text(
+            'U&I',
+            style: textTheme.displayLarge,
+          )
+        ]));
+  }
+}
+
+class _MiddlePart extends StatelessWidget {
   final DateTime selectedDate;
   final VoidCallback onPressed;
+  final VoidCallback onTapMsg;
+  final String message;
 
-  _TopPart({
+  _MiddlePart({
     required this.selectedDate,
     required this.onPressed,
+    required this.onTapMsg,
+    required this.message,
     Key? key,
   }) : super(key: key);
 
@@ -87,18 +166,18 @@ class _TopPart extends StatelessWidget {
     final now = DateTime.now();
 
     return Expanded(
+      flex: 2,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text(
-            'U&I',
-            style: textTheme.displayLarge,
-          ),
           Column(
             children: [
-              Text(
-                '우리 처음 만난날',
-                style: textTheme.bodyMedium,
+              GestureDetector(
+                onTap: onTapMsg,
+                child: Text(
+                  message == "" ? "클릭하여 메시지 입력" : message,
+                  style: textTheme.bodyMedium,
+                ),
               ),
               Text(
                 '${selectedDate.year}.${selectedDate.month}.${selectedDate.day}',
@@ -133,10 +212,6 @@ class _BottomPart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Image.asset(
-        'asset/img/middle_image.png',
-      ),
-    );
+    return Expanded(child: Text(""));
   }
 }
